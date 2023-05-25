ec5e4dc7bc1141f3e17c38d4d0688407a34e8bf9
Efrain Daza <dazaa@hotmail.com>
2023-05-24 22:29:38 -0300

    el verdadero mercado liebre

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+app.use(express.static("public"));
 
-const publicPath = path.resolve (__dirname, "./public") ;
-app.use(express.static(publicPath));
-
-app.listen(3030, () => {
-    console.log("servidor corriendo en puerto 3030")
-});
-
-app.get("/", (req,res) => {
-    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("servidor corriendo en el puerto 3000")
 })
+
+app.get("/", function (req,res){
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+
+app.get("/register", function (req,res){
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+
+app.get("/login", function (req,res){
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+
+
